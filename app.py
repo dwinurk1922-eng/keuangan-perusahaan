dfde6d6690693277a65ad7794e254123f4a6d54a
dwinurk1922-eng <dwinurk1922@gmail.com>
2026-07-06 11:40:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from streamlit_js_eval import streamlit_js_eval
 
 # =========================================================================
 # PENGATURAN DATABASE & AI ANDA (SUDAH DIKODEKAN LANGSUNG)
-SHEETS_URL = "https://docs.google.com/spreadsheets/d/1VDqISHpjg8OWWPzl1NWOcc9V6j_o6Zw2/edit?usp=sharing"
+SHEETS_URL = "https://docs.google.com/spreadsheets/d/1VDqISHpjg8OWWPzl1NWOcc9V6j_o6Zw2/edit?usp=sharing&ouid=117398658595436431688&rtpof=true&sd=true"
 GEMINI_API_KEY = "AQ.Ab8RN6J6P_ygWhv1BVnR7cZDTwU4F3bhuTPKXHi1BB_ZzUikGg"
 
 # KOORDINAT PUSAT KANTOR PT TANGGUH CAHAYA PRATAMA (Silakan sesuaikan koordinat asli kantor Anda)
